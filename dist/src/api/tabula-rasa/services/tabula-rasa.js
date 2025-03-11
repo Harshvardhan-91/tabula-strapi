48e450416d7fbc38c71a4f803d82360162f5a6c6
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-03-12 00:43:52 +0530

    added changes

---
+++ dist/src/api/tabula-rasa/services/tabula-rasa.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * tabula-rasa service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::tabula-rasa.tabula-rasa');
