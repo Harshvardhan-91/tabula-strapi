b4131e3c5f1483e3918a4d39e39386f622bb28fd
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-03-12 15:16:33 +0530

    added changes

--- dist/config/server.js
+++ dist/config/server.js
@@ -3,7 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
+    url: env('PUBLIC_URL'),
     app: {
         keys: env.array('APP_KEYS'),
+    },
+    admin: {
+        auth: {
+            secret: env('ADMIN_JWT_SECRET', 'your-default-secret-here'),
+        },
     },
 });
